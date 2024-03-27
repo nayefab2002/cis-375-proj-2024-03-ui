c5d8dbf993edbbe01df55899ff275a2507066e74
Nayef <nayefabdullah111@gmail.com>
2024-03-27 16:46:54 -0400

    SideNavigation added

---
+++ app/Dashboard/layout.tsx
@@ -0,0 +1,12 @@
+import SideNavigation from "@/app/Utils/sideNav";
+ 
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNavigation />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+  );
+}
