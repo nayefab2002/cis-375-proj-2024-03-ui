7101c29901163c51b847cd59289aa9380fbb16a1
Ryan Allen <83918018+RyanAIIen@users.noreply.github.com>
2024-04-02 02:14:29 -0400

    Add GH Action health check workflow (#2)

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,10 @@ export default function Home() {
         <h1 className='text-3xl'>CIS-375 Hotel Management UI</h1>
         <p>Coming Soon.</p>
         <p>
-          <a href='https://github.com/RyanAIIen/cis-375-proj-2024-03-ui'>
+          <a
+            target='_blank'
+            href='https://github.com/RyanAIIen/cis-375-proj-2024-03-ui'
+          >
             See the Code
           </a>
         </p>
