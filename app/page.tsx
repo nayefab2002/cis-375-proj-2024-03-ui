a27ff78703c367a76406d795239b933db416eb5c
Nayef <nayefabdullah111@gmail.com>
2024-04-02 21:44:12 -0400

    Reservation Page UI added

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Login from "./Login/page";
-import DashBoardPage from "./UI/Dashboard/page";
+import DashBoardPage from "./Dashboard/page";
 export default function Home() {
   return <Login/>;
   //return <DashBoardPage/>;
